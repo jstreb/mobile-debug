1313f5e32c6ac6780e85613b234b1f8a9c633c26
Jesse Streb <jstreb@brightcove.com>
2012-04-06 16:03:28 -0400

    adding some appcloud touches

---
+++ web/client/appcloud.js
@@ -0,0 +1,8 @@
+$( document ).ready() {
+  
+  var id = location.href.split( "#" )[1];
+  var url = location.href.split( "client" )[0] + "target/target-script-min.js#" + id;
+  
+  $( "body" ).append( '<img src="https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=' + url + '" />' );
+  
+};
