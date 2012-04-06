6dbdcd1b85209110280b31a0cbd89950fbaccdff
Jesse Streb <jstreb@brightcove.com>
2012-04-06 16:18:27 -0400

    more

--- web/client/appcloud.js
+++ web/client/appcloud.js
@@ -3,6 +3,6 @@ $( document ).ready( function() {
   var id = location.href.split( "#" )[1];
   var url = location.href.split( "client" )[0] + "target/target-script-min.js#" + id;
   
-  $( "body" ).append( '<img src="https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=' + url + '" />' );
+  $( "body" ).append( '<img class="qr" src="https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=' + url + '" />' );
   
 });
