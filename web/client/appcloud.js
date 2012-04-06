378e12e94bef5402fb4c39ddc7a5a972780e1d2b
Jesse Streb <jstreb@brightcove.com>
2012-04-06 16:15:11 -0400

    fixing typo

--- web/client/appcloud.js
+++ web/client/appcloud.js
@@ -1,8 +1,8 @@
-$( document ).ready() {
+$( document ).ready( function() {
   
   var id = location.href.split( "#" )[1];
   var url = location.href.split( "client" )[0] + "target/target-script-min.js#" + id;
   
   $( "body" ).append( '<img src="https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=' + url + '" />' );
   
-};
+});
