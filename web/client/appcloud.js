f468d83dac8e3c6ab012fa3330ca79efbc94e528
Jesse Streb <jstreb@brightcove.com>
2012-04-09 11:33:55 -0400

    had to use a delimiter other then a hash

--- web/client/appcloud.js
+++ web/client/appcloud.js
@@ -1,7 +1,7 @@
 $( document ).ready( function() {
   
   var id = location.href.split( "#" )[1];
-  var url = encodeURI( location.href.split( "client" )[0] + "target/target-script-min.js+++" + id );
+  var url = encodeURI( location.href.split( "client" )[0] + "target/target-script-min.jshash" + id );
   $( "body" ).append( '<img class="qr" src="https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=' + url + '" />' );
   
 });
