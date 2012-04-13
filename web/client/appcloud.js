380127cdb5b9e5c767c3b4c9fd22dffa936c78a5
Jesse Streb <jstreb@brightcove.com>
2012-04-13 13:34:38 -0400

    adding some hotness

--- web/client/appcloud.js
+++ web/client/appcloud.js
@@ -2,6 +2,8 @@ $( document ).ready( function() {
   
   var id = location.href.split( "#" )[1];
   var url = encodeURI( location.href.split( "client" )[0] + "target/target-script-min.jshash" + id );
-  $( "body" ).append( '<img class="qr" src="https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=' + url + '" />' );
-  
+  $( "body" ).append( '<img class="qr" src="https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=' + url + '" /><img class="reaction" src="http://i.imgur.com/eQsuN.gif" />' )
+             .on( "click", "li", function() {
+               $( ".reaction" ).addClass( "show" );
+             });
 });
